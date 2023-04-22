8c6c2418e0d2ead6f16876bb443d2babc5565a33
Chandrababugowda <chandrababugowda3042003@gmail.com>
2023-04-22 14:51:06 +0530

    Add gallery background and make the carousel

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 const slides = document.querySelectorAll(".slide");
+const reviewSlidesArr = [
+  document.querySelectorAll(".review-slide-1"),
+  document.querySelectorAll(".review-slide-2"),
+  document.querySelectorAll(".review-slide-3"),
+  document.querySelectorAll(".review-slide-4"),
+];
 const nextSlide = document.querySelector(".btn-next");
 const prevSlide = document.querySelector(".btn-prev");
+const hamBtn = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
 
 let curSlide = 0;
 let maxSlide = slides.length - 1;
+let reviewCurrentSlide = 0;
+// let reviewMaxSlide = reviewSlides.length - 1;
+
+// *************** Hamburger ***************
+
+// hamBtn.addEventListener("click", () => {
+//   console.log("clicked");
+
+//   if (navLinks.classList.contains("hidden")) {
+//     navLinks.classList.remove("hidden");
+//   } else {
+//     navLinks.classList.add("hidden");
+//   }
+// });
+
+// *************** Carousel ***************
 
 slides.forEach((slide, index) => {
   slide.style.transform = `translateX(${index * 100}%)`;
+});
+
+reviewSlidesArr.forEach((reviewSlides) => {
+  reviewSlides.forEach((reviewSlide, index) => {
+    reviewSlide.style.transform = `translateX(${index * 100}%)`;
+  });
 });
 
 automatic();
@@ -18,8 +48,15 @@ function automatic() {
     curSlide++;
   }
 
-  slides.forEach((slide, index) => {
-    slide.style.transform = `translateX(${100 * (index - curSlide)}%)`;
+  if (reviewCurrentSlide == 3) {
+    reviewCurrentSlide = 0;
+  } else {
+    reviewCurrentSlide++;
+  }
+
+  changeSlide(slides, curSlide);
+  reviewSlidesArr.forEach((reviewSlides) => {
+    changeSlide(reviewSlides, reviewCurrentSlide);
   });
   setTimeout(automatic, 3500);
 }
@@ -30,10 +67,7 @@ nextSlide.addEventListener("click", () => {
   } else {
     curSlide++;
   }
-
-  slides.forEach((slide, index) => {
-    slide.style.transform = `translateX(${100 * (index - curSlide)}%)`;
-  });
+  changeSlide(slides, curSlide);
 });
 
 prevSlide.addEventListener("click", () => {
@@ -42,8 +76,11 @@ prevSlide.addEventListener("click", () => {
   } else {
     curSlide--;
   }
-
-  slides.forEach((slide, index) => {
-    slide.style.transform = `translateX(${100 * (index - curSlide)}%)`;
-  });
+  changeSlide(slides, curSlide);
 });
+
+function changeSlide(slidesArr, currentSlide) {
+  slidesArr.forEach((slide, index) => {
+    slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`;
+  });
+}
